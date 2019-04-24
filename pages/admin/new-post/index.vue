abeb29dbccb05c0974c672d3bc69caf5d54f1626
mfcarneiro <matheus.felipe123@gmail.com>
2019-04-24 06:58:35 -0300

    Add the New-post page
    Created the new post page within admin area.

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,9 +1,49 @@
-<template>
-	<div></div>
-</template>
-
 <script>
 export default {};
 </script>
+
+<template>
+	<section>
+		<v-container fluid>
+			<v-layout
+				align-content-space-around
+				justify-space-around
+				fill-height
+				row
+			>
+				<v-flex>
+					<v-card>
+						<v-card-title primary-title>
+							<h2 class="headline mb-0">
+								New Post!
+							</h2>
+						</v-card-title>
+						<v-container>
+							<v-layout justify-center column fill-height>
+								<v-flex xs12>
+									<v-layout row wrap>
+										<v-flex xs12>
+											<v-text-field label="Author name">
+											</v-text-field>
+											<v-text-field label="Title"> </v-text-field>
+											<v-text-field label="Thumbnail link">
+											</v-text-field>
+											<v-textarea
+												auto-grow
+												no-resize
+												label="Content"
+											>
+											</v-textarea>
+										</v-flex>
+									</v-layout>
+								</v-flex>
+							</v-layout>
+						</v-container>
+					</v-card>
+				</v-flex>
+			</v-layout>
+		</v-container>
+	</section>
+</template>
 
 <style scoped></style>
